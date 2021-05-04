a2b113340dc13a2b3e667eb52f7d75e64cba6344
Ben Smith <ben@smithsrus.com>
2021-05-04 16:00:52 -0500

    test first game condition

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -1,2 +1,9 @@
 require 'rspec'
 require './lib/rps.rb'
+
+describe '' do
+  it 'returns true if rock is the object and scissors is the argument' do
+    game = RPS.new
+    expect(game.wins?("rock", "scissors")).to eq(true)
+  end
+end
