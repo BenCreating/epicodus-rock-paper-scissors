3da7d0f69a62fe6b6c7ad9b00e578a06910874c1
Ben Smith <ben@smithsrus.com>
2021-05-04 16:50:17 -0500

    add tests for every possible move combination

--- spec/rps_spec.rb
+++ spec/rps_spec.rb
@@ -2,8 +2,51 @@ require 'rspec'
 require './lib/rps.rb'
 
 describe '' do
+  # Rock tests
+  it 'returns false if rock is the object and rock is the argument' do
+    game = RPS.new
+    expect(game.wins?("rock", "rock")).to eq(false)
+  end
+
+  it 'returns false if rock is the object and paper is the argument' do
+    game = RPS.new
+    expect(game.wins?("rock", "paper")).to eq(false)
+  end
+
   it 'returns true if rock is the object and scissors is the argument' do
     game = RPS.new
     expect(game.wins?("rock", "scissors")).to eq(true)
+  end
+
+  # Paper tests
+  it 'returns true if paper is the object and rock is the argument' do
+    game = RPS.new
+    expect(game.wins?("paper", "rock")).to eq(true)
+  end
+
+  it 'returns false if paper is the object and paper is the argument' do
+    game = RPS.new
+    expect(game.wins?("paper", "paper")).to eq(false)
+  end
+
+  it 'returns false if paper is the object and scissors is the argument' do
+    game = RPS.new
+    expect(game.wins?("paper", "scissors")).to eq(false)
+  end
+
+  # Scissors tests
+  it 'returns false if scissors is the object and rock is the argument' do
+    game = RPS.new
+    expect(game.wins?("scissors", "rock")).to eq(false)
+  end
+
+  it 'returns true if scissors is the object and paper is the argument' do
+    game = RPS.new
+    expect(game.wins?("scissors", "paper")).to eq(true)
+  end
+
+  it 'returns false if scissors is the object and scissors is the argument' do
+    game = RPS.new
+    expect(game.wins?("scissors", "scissors")).to eq(false)
   end
 end
